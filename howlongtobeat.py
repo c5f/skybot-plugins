ad4a465dd85beec6537ed2e6badfa6a781590597
Colin Scott-Fleming <colinsf@arizona.edu>
2013-03-10 11:51:45 -0700

    Console testing saves a lot of commits.

--- howlongtobeat.py
+++ howlongtobeat.py
@@ -20,7 +20,7 @@ def how_long(inp):
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
- 
+
     gamelist = etree.HTML(
         session.post(
             base_url + gamelist, 
@@ -30,16 +30,35 @@ def how_long(inp):
     )
 
     gamebreakdown = gamelist.xpath(
-        "//div[@id='gamelist_list']/div[1]/a[@title='Full Game Page']/a")
+        "//div[@id='gamelist_list']/div[1]/a[@title='Full Game Page']/@href")[0]
 
-    #html = etree.HTML(
-    #    session.get(
-    #       base_url + gamebreakdown,
-    #       headers=headers
-    #    ).text
-    #)
+    html = etree.HTML(
+       session.get(
+          base_url + gamebreakdown,
+          headers=headers
+       ).text
+    )
 
-    return gamelist
+    title = html.xpath(
+        "//div[@class='headermain']/text()")[0]
+
+    params = {"url": base_url + gamebreakdown}
+
+    link = session.get(
+        "http://tinyurl.com/api-create.php?", params).text
+
+    table = html.xpath("//div[@class='gamepage_flow']/table")[0]
+
+    main_time = table.xpath("//tbody[2]/tr/td[4]/text()")[0]
+    extra_time = table.xpath("//tbody[3]/tr/td[4]/text()")[0]
+    complete_time = table.xpath("//tbody[4]/tr/td[4]/text()")[0]
+    overall = table.xpath("//tbody[5]/tr/td[4]/text()")[0]
+
+    outp = "How long to beat %s (%s) (averages)\n" % (title, link)
+    outp += "Main Story: %s, Main + Extras: %s, Completionist: %s, Overall %s" % (
+        main_time, extra_time, complete_time, overall)
+
+    return outp
 
 @hook.command('hltb')
 @hook.command('howlong')
